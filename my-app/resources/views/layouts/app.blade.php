ada54c5434ce83ca51fb6b9547234d0f8f8f05a0
cozy-666 <cozy.dev.tech@gmail.com>
2026-02-05 23:55:39 +0900

    レイアウト設定

---
+++ my-app/resources/views/layouts/app.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>@yield('title')</title>
+</head>
+
+<body>
+    <header>
+    <h1>イノセントSNSアプリ</h1>
+    </header>
+    <div class='content'>
+        @yield('content')
+    </div>
+    <footer>
+        <p>@ 2026 イノセントSNSアプリ</p>
+    </footer>  
+</body>
+
+</html>
--- my-app/resources/views/posts/index.blade.php
+++ my-app/resources/views/posts/index.blade.php
@@ -1,18 +1,14 @@
-<!DOCTYPE html>
-<html>
+@extends('layouts.app')
 
-<head>
-    <title>投稿一覧</title>
-</head>
+@section('title','投稿一覧')
 
-<body>
-    <h1>投稿一覧</h1>
-    @foreach ($posts as $post)
-    <div>
-        <h2>{{ $post->title }}</h2>
-        <p>{{ $post->body }}</p>
-    </div>
-    @endforeach
-</body>
+@section('content')
+<h1>投稿一覧</h1>
+@foreach ($posts as $post)
+<div>
+    <h2>{{ $post->title }}</h2>
+    <p>{{ $post->body }}</p>
+</div>
+@endforeach
+@endsection
 
-</html>
