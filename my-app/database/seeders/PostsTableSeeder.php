408f746bb30fce76d97eb1a4bf9d89c91484177b
cozy-666 <cozy.dev.tech@gmail.com>
2026-02-06 22:38:49 +0900

    シーダー準備

--- my-app/database/seeders/PostsTableSeeder.php
+++ my-app/database/seeders/PostsTableSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class PostsTableSeeder extends Seeder
 {
@@ -12,6 +13,12 @@ class PostsTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('posts')->insert([
+            'user_id' => 1,
+            'title' => 'サンプル投稿',
+            'body' => 'これはサンプルの投稿内容です。',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
     }
 }
