d358a81e3f7133cbb1b774f07fb949f4299b3b27
cozy-666 <cozy.dev.tech@gmail.com>
2026-02-07 12:23:54 +0900

    DBファサード作ってDB表示確認

--- my-app/app/Models/Post.php
+++ my-app/app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Post extends Model
 {
@@ -16,5 +17,12 @@ class Post extends Model
         $post->content = $data->content;
         $post ->save();
         return $post;
+    }
+
+    public function GetPostWithNormalSql()
+    {
+        $posts = DB::select('SELECT * FROM posts');
+        dd($posts);
+        return $posts;
     }
 }
