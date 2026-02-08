0cc294def8e23783aad5a2b0b13e0ce42518f18c
cozy-666 <cozy.dev.tech@gmail.com>
2026-02-08 13:50:08 +0900

    php artisan make:migration add_deleted_at_to_posts_table --table=posts

--- my-app/app/Models/Post.php
+++ my-app/app/Models/Post.php
@@ -4,11 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Facades\DB;
 
 class Post extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
     public function user()
     {
