b27f86bc7eeb463009a0ad9f5b1951331a2107eb
cozy-666 <cozy.dev.tech@gmail.com>
2026-02-08 17:17:06 +0900

    変更できるカラムをあらかじめ設定する

--- my-app/app/Models/Post.php
+++ my-app/app/Models/Post.php
@@ -12,6 +12,11 @@ class Post extends Model
     use HasFactory;
     use SoftDeletes;
 
+    protected $fillable = [
+        'title',
+        'body',
+    ];
+
     public function user()
     {
         return $this->belongTo(User::class);
